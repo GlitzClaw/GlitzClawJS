a04bac193909773aef5daae451ef9e50bd4e4990
GlitzClaw <glitzclaw@gmail.com>
2015-12-17 03:12:23 -0500

    Added greeting function.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 
+function myFunction() {
+  //welcome script
+  //To do: Change Cancel to "no" 
+var r = confirm("Welcome! Do you like the framework?");
+if (r == true) {
+    x = "Good!";
+} else {
+    x = "Sorry!";
+} 
+}
