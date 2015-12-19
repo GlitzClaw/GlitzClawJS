eb717371d7fe64a3ec3385f5d8dfec3d14aa6a9c
GlitzClaw <glitzclaw@gmail.com>
2015-12-19 23:25:04 +0000

    Settings.sh
    Configure build for GlitzClawJS in ShellScript.

---
+++ settings.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+echo "menu outline by Dennis Williamson" 
+echo "http://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script"
+PS3='Please enter your choice: '
+options=("Remove CSS" "Remove index.html" "Remove JS" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Remove CSS")
+            rm -fr css
+            ;;
+        "Remove index.html")
+            rm -fr index.html
+            ;;
+        "Remove JS")
+            rm -fr js
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
